6e593105478721294853437dd80b3ef5d771c0d8
sleepntsheep <contact@papangkorn.com>
2022-09-22 14:44:25 +0700

    move example

--- sslide.c
+++ sslide.c
@@ -50,15 +50,14 @@
 
 static void _die(int line_number, const char * format, ...)
 {
+    fprintf(stderr, "%d: ", line_number);
     va_list vargs;
     va_start (vargs, format);
-    fprintf (stderr, "%d: ", line_number);
-    vfprintf (stderr, format, vargs);
-    fprintf (stderr, ".\n");
+    vfprintf(stderr, format, vargs);
     va_end (vargs);
-    exit (1);
+    fprintf(stderr, "\n");
+    abort();
 }
-
 
 enum FrameType {
     FRAMETEXT,
